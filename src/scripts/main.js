8c0f5e66958ddfd8db5eb6b1ebd929b3e7d8650e
Horoshko Vitaliy <horoshko29@gmail.com>
2025-11-25 05:43:11 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,30 @@
 'use strict';
 
-const food = {
-  Drink: {
-    Wine: {},
-    Schnaps: {},
-  },
+export function createTree(element, data) {
+  element.innerHTML = '';
 
-  Fruit: {
-    Red: {
-      Cherry: {},
-      Strawberry: {},
-    },
-    Yellow: {
-      Banana: {},
-      Pineapple: {},
-    },
-  },
-};
+  const ul = document.createElement('ul');
 
-const tree = document.querySelector('#tree');
+  for (const key in data) {
+    const value = data[key];
+    const li = document.createElement('li');
 
-function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+    const hasChildren =
+      value && typeof value === 'object' && Object.keys(value).length > 0;
+
+    if (hasChildren) {
+      li.textContent = `◦ ${key}`;
+
+      const nestedUl = document.createElement('ul');
+
+      li.appendChild(nestedUl);
+      createTree(nestedUl, value);
+    } else {
+      li.textContent = `• ${key}`;
+    }
+
+    ul.appendChild(li);
+  }
+
+  element.appendChild(ul);
 }
-
-createTree(tree, food);
